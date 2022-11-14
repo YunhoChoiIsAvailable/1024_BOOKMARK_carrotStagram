6756b561b7f80fb3edaa6773af53e5de7b66424b
YunhoChoi <choiyunho0503@gmail.com>
2022-11-14 17:10:43 +0900

    Likes, Comments, CommentLikes in models, password, url까지 적은 plan html까지 추가 friend, follower, following

--- carrotStagram/models.py
+++ carrotStagram/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Account(models.Model):
-    name = models.CharField(max_length=50)
+    name = models.CharField(max_length=50, unique=True)
     description = models.CharField(max_length=100)
     account_img = models.ImageField(upload_to='profiles_imgs')
     follow_set = models.ManyToManyField('self', blank = True)
