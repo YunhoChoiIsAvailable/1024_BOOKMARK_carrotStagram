166ef692e6f0441a98189e5ba32d42b64e92021f
YunhoChoi <choiyunho0503@gmail.com>
2022-11-14 09:37:44 +0900

    Likes, Comments, CommentLikes in models

--- carrotStagram/models.py
+++ carrotStagram/models.py
@@ -13,8 +13,22 @@ class Post(models.Model):
     description = models.CharField(max_length=100)
     link = models.URLField(max_length=200)
     uploader = models.ForeignKey(Account, on_delete=models.CASCADE)
-    like = models.ManyToManyField(Account)
     post_img = models.ImageField(upload_to='post_imgs')
     created_dt = models.DateTimeField(auto_now_add = True)
     modified_dt = models.DateTimeField(auto_now = True)
+
+class Likes(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    count = models.IntegerField(max_length=10)
+    people = models.ManyToManyField(Account, on_delete=models.CASCADE)
+
+class Comments(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    person = models.ManyToManyField(Account, on_delete=models.CASCADE)
+    content = models.CharField(max_length=500)
+
+class CommentLikes(models.Model):
+    comment = models. ForeignKey(Comments, on_delete=models.CASCADE)
+    people = models.ManyToManyField(Account, on_delete=models.CASCADE)
+    count = models.IntegerField(max_length=10)
 
