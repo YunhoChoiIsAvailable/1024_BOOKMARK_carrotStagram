3491e566ec20db7f8bb2a45ef35eef6a26158b96
jhwan-g <doveuyhtgfedr@gmail.com>
2022-11-17 16:22:22 +0900

    .

--- carrotStagram/admin.py
+++ carrotStagram/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from carrotStagram.models import *
 
 # Register your models here.
+admin.site.register(Account)
+admin.site.register(Post)
+admin.site.register(Comments)
+admin.site.register(Likes)
+admin.site.register(CommentLikes)
