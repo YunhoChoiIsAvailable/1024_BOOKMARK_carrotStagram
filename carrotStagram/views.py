d8f0a06eca46c138f93c526e8c6a4e6104abc328
jhwan-g <doveuyhtgfedr@gmail.com>
2022-11-17 16:21:59 +0900

    view 및 url 수정 - view ; friend post 보여주는 부분 넣음 -> 수정 필요

--- carrotStagram/views.py
+++ carrotStagram/views.py
@@ -1,23 +1,25 @@
 from django.shortcuts import render, HttpResponseRedirect, reverse
 from django.views.generic import *
 from carrotStagram.models import *
+from django.contrib import auth
 
 # Create your views here.
 def loginView(request):
     if request.method == 'GET':
         return render(request, 'carrotStagram/login.html')
     elif request.method == 'POST':
-        accountname = request.POST.get('accountname', None)
-        password = request.POST.get('accountpw', None)
+        username = request.POST.get('username', None)
+        password = request.POST.get('userpw', None)
 
+    print(username, password)
     error_message = {}
-    if not (accountname and password):
+    if not (username and password):
         error_message['error'] = "올바르지 않은 id/pw입니다"
     else:
         try:
-            account = Account.objects.get(name=accountname)
+            account = Account.objects.get(name=username)
             if password == account.password:
-                request.session ['user'] = account.id
+                request.session['user'] = account.id
                 return HttpResponseRedirect(reverse('carrotstagram:friend'))
             else:
                 error_message['error']='올바르지 않은 id/pw입니다'
@@ -26,10 +28,27 @@ def loginView(request):
     return render(request,'carrotStagram/login.html', error_message)
 
 def settingView(request):
+    id = request.session['user']
+
+def get_friend_list():
     pass
 
-class FriendView(ListView):
+def get_friend_posts():
     pass
+
+class FriendView(TemplateView):
+    template_name = 'carrotStagram/friend.html'
+    def get(self, request):
+        id = request.session['user']
+        account = Account.objects.get(pk=id)
+        posts = []
+        for follow_account in account.follows.all():
+            for post in follow_account.post_set.all():
+                posts.append(post)
+        posts = sorted(posts, key = (lambda post : post.modified_dt))
+        print(posts)
+        context = {'posts' : posts}
+        return self.render_to_response(context)
 
 class FeedView(ListView):
     pass
