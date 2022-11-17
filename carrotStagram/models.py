9b6c917fbdf2c688e6efb99058b2b2a48dfd2951
jhwan-g <doveuyhtgfedr@gmail.com>
2022-11-17 16:21:06 +0900

    account model 수정, migration 함

--- carrotStagram/models.py
+++ carrotStagram/models.py
@@ -4,16 +4,15 @@ from django.db import models
 class Account(models.Model):
     name = models.CharField(max_length=50, unique=True)
     description = models.CharField(max_length=100)
-    account_img = models.ImageField(upload_to='profiles_imgs')
-    follow_set = models.ManyToManyField('self', blank = True)
-    following_set = models.ManyToManyField('self', blank = True)
-    password = models.CharField(max_length=50)
+    account_img = models.ImageField(upload_to='profiles_imgs', blank = True)
+    follows = models.ManyToManyField('self', blank = True, symmetrical=False, related_name='account_followers')
+    password = models.CharField(max_length=50, blank = False, default='0000')
 
 
 class Post(models.Model):
     name = models.CharField(max_length=50)
     description = models.TextField()
-    link = models.URLField(max_length=200)
+    link = models.URLField(max_length=200, blank = True)
     uploader = models.ForeignKey(Account, on_delete=models.CASCADE)
     post_img = models.ImageField(upload_to='post_imgs')
     created_dt = models.DateTimeField(auto_now_add = True)
@@ -21,7 +20,7 @@ class Post(models.Model):
 
 class Likes(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
-    count = models.IntegerField(max_length=10)
+    count = models.IntegerField()
     people = models.ManyToManyField(Account)
 
 class Comments(models.Model):
@@ -32,5 +31,4 @@ class Comments(models.Model):
 class CommentLikes(models.Model):
     comment = models. ForeignKey(Comments, on_delete=models.CASCADE)
     people = models.ManyToManyField(Account)
-    count = models.IntegerField(max_length=10)
-
+    count = models.IntegerField()
